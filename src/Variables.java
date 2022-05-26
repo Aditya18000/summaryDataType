dedaa6303727891e4f488d37727e77cd8daaeb7a
Aditya18000 <106184778+Aditya18000@users.noreply.github.com>
2022-05-26 14:49:11 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,8 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
- * version:
+ * author: Aditya Patel
+ * date: 05/26/2022
+ * version: 18
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
  * direction: 1) read the source material
@@ -10,11 +10,27 @@
  *            double, boolean, char (2 lines per each type)
  *
  */
-public class Variables
+public class Variable
 {
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("Byte data type has negative value of -128 and maximum of 127");
+        System.out.println("Byte is also use to save space in memory and is a 8 bit raised by a factor of 2");
+        System.out.println("The short data type represents a 16 bit therefore it can save more memory");
+        System.out.println("It also has 65536 values which is double bit than byte");
+        System.out.println("Int data type is 32 bit therefore more values and is double the bit of short");
+        System.out.println("Int data type is used when there needs to be more variables than short data type");
+        System.out.println("Long data type is used when int can not provide enough values");
+        System.out.println("It is double bit of int therefore its 64 bit");
+        System.out.println("Float data type cannot be used for precise data types such as currency");
+        System.out.println("Float data type is used to save memory in large floating point numbers");
+        System.out.println("The double data type is generally used with decimal values");
+        System.out.println("The double data type should not be used for precise values");
+        System.out.println("The boolean data type has only two values, true and false");
+        System.out.println("The boolean data type has no precise size");
+        System.out.println("The char data type has a maximum value of 65536 values");
+        System.out.println("The char data type has a minimum value of zero");
     }
 
 }
